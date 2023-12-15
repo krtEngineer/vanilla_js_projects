35284dae4043f543ae90fbcf4ba99ec11de43218
krtEngineer <krt.engineer@gmail.com>
2023-12-15 17:55:19 +0530

    Adding JS functions.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

---
+++ counter_oops/app.js
@@ -0,0 +1,48 @@
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+  /**
+   * this.decreaseBtn.addEventListener("click", this.decrease);
+   * this.decrease points to its caller i.e. button
+   * this.decrease.bind(this) now points to Counter function
+   */
+  this.increase = this.increase.bind(this);
+  this.reset = this.reset.bind(this);
+  this.decrease = this.decrease.bind(this);
+  this.decreaseBtn.addEventListener("click", this.decrease);
+  this.resetBtn.addEventListener("click", this.reset);
+  this.increaseBtn.addEventListener("click", this.increase);
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists.`
+  );
+}
