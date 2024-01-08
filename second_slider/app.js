dfb5a3e89b74be99878b3a80da5c0506e19a625a
krtEngineer <krt.engineer@gmail.com>
2024-01-08 12:30:42 +0530

    Adding JS functions.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

---
+++ second_slider/app.js
@@ -0,0 +1,68 @@
+import people from "./data.js";
+const slideContainer = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+// set slides
+slideContainer.innerHTML = people
+  .map((person, slideIndex) => {
+    const { img, name, job, text } = person;
+    let position = "next";
+    if (slideIndex === 0) {
+      position = "active";
+    }
+    if (slideIndex === people.length - 1) {
+      position = "last";
+    }
+    return ` <article class="slide ${position}">
+          <img
+            src="${img}"
+            class="img"
+            alt="Kamil"
+          />
+          <h4>${name}</h4>
+          <p class="title">${job}</p>
+          <p class="text">
+           ${text}
+          </p>
+          <div class="quote-icon">
+            <div class="fas fa-quote-right"></div>
+          </div>
+        </article>`;
+  })
+  .join("");
+
+const startSlider = (type) => {
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+  if (!next) {
+    next = slideContainer.firstElementChild;
+  }
+  active.classList.remove(["active"]);
+  last.classList.remove(["last"]);
+  next.classList.remove(["next"]);
+
+  if (type === "prev") {
+    active.classList.add("next");
+    last.classList.add("active");
+    next = last.previousElementSibling;
+    if (!next) {
+      next = slideContainer.lastElementChild;
+    }
+    next.classList.add("last");
+    return;
+  }
+
+  active.classList.add("last");
+  last.classList.add("next");
+  next.classList.add("active");
+};
+
+nextBtn.addEventListener("click", () => {
+  startSlider("");
+});
+
+prevBtn.addEventListener("click", () => {
+  startSlider("prev");
+});
