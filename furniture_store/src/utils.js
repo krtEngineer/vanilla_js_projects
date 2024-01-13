29b373fa4e73d62b356ca44e29adcf99bb8a7c62
krtEngineer <krt.engineer@gmail.com>
2024-01-14 00:44:56 +0530

    Adding JS functions.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

---
+++ furniture_store/src/utils.js
@@ -0,0 +1,40 @@
+const allProductsUrl = "https://course-api.com/javascript-store-products";
+const singleProductUrl =
+  "https://course-api.com/javascript-store-single-product";
+
+const getElement = (selection) => {
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
+};
+
+const formatPrice = (price) => {
+  const newPrice = +price * 85;
+  const inrPrice = newPrice.toLocaleString("en-IN");
+  return `&#8377;${inrPrice}`;
+};
+
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item));
+  } else {
+    storageItem = [];
+  }
+  return storageItem;
+};
+
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
+
+export {
+  allProductsUrl,
+  singleProductUrl,
+  getElement,
+  formatPrice,
+  getStorageItem,
+  setStorageItem,
+};
